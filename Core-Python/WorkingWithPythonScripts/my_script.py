bb53f8e40142cd67f459efa2ada93a2a2bfec26b
Abhisek <abhiseknayak169@gmail.com>
2025-04-19 22:18:48 +0530

    psuh

---
+++ Core-Python/WorkingWithPythonScripts/my_script.py
@@ -0,0 +1,17 @@
+x = 100
+print(x)
+
+def my_function():
+    print("Hello from my_function!")
+
+print(__name__)
+
+if __name__ == "__main__":
+    my_function()
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    print("The sum of the two numbers is: ")
+    print(a + b)
+
+
+
